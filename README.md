9800aad8e2d4c3fc197584d698343ac0a0612cdc
Philipp Sehnert <psikon86@gmail.com>
2013-03-20 14:19:49 +0100

    Readme updated

--- README.md
+++ README.md
@@ -8,7 +8,21 @@
 
 ## Installation
 
+The pipeline provide an automatic installer script in the main folder. To install all necassary components for the pipeline just start this skript in a unix shell by the following command.
+
+'''bash
+sh install.sh
+'''
+
+All necassary programs will be downloaded and make runable automatically.
+
 ## Usage
+
+## Working
+
+
+## not Working
+
 
 ## Tools in the Pipeline
 	
--- run.py
+++ run.py
@@ -18,61 +18,64 @@ def preProcessing(run):
         # create an PreProcessing Object
         pre = PreProcessing()
         
-        if helperInstance.checkParameter('PreProcessing', 'quality check',"parameter.txt") == 'yes':
-            pre.qualityCheck(infile,threads)  
+        if helperInstance.checkParameter('PreProcessing', 'quality check', "parameter.txt") == 'yes':
+            pre.qualityCheck(infile, threads)  
         
-        if helperInstance.checkParameter('PreProcessing', 'trimming',"parameter.txt") == 'yes':
-            pre.qualityTrimming(infile, threads,"parameter.txt")
+        if helperInstance.checkParameter('PreProcessing', 'trimming', "parameter.txt") == 'yes':
+            pre.qualityTrimming(infile, threads, "parameter.txt")
             
     else:
         print ("skip Step:", skip)
 
 def assembly(run):
 
-    if run ==True: 
+    if run == True: 
         assemble = Assembly()
-        if helperInstance.checkParameter('Assembly', 'program',"parameter.txt") == 'metavelvet':
-           assemble.metavelvet(infile,threads,"parameter.txt")
+        if helperInstance.checkParameter('Assembly', 'program', "parameter.txt") == 'metavelvet':
+           assemble.metavelvet(infile, threads, "parameter.txt")
            
-        elif helperInstance.checkParameter('Assembly', 'program',"parameter.txt") == 'abyss':
+        elif helperInstance.checkParameter('Assembly', 'program', "parameter.txt") == 'abyss':
             print 'starte abyss'
-        elif helperInstance.checkParameter('Assembly', 'program',"parameter.txt") == 'metaidba':
+        elif helperInstance.checkParameter('Assembly', 'program', "parameter.txt") == 'metaidba':
             print 'starte metaidba'
-        elif helperInstance.checkParameter('Assembly', 'program',"parameter.txt") == 'stitch':
+        elif helperInstance.checkParameter('Assembly', 'program', "parameter.txt") == 'stitch':
             print "starte stitch"
         else:
             print 'no assembly program recognized! Please check parameter file!'
     else:
-        print "skip Step:"+skip
+        print "skip Step:" + skip
 
 def annotate(run):
-    if run ==True: 
-        if helperInstance.checkParameter('Annotate', 'program',"parameter.txt") == 'blastn':
+    
+    if run == True: 
+        if helperInstance.checkParameter('Annotate', 'program', "parameter.txt") == 'blastn':
             print 'starte blastn'
-        elif helperInstance.checkParameter('Annotate', 'program',"parameter.txt") == 'metacv':
+        elif helperInstance.checkParameter('Annotate', 'program', "parameter.txt") == 'metacv':
             print 'starte metacv'
         else:
             print 'no assembly program recognized! Please check parameter file!'
     else:
-        print "skip Step:"+skip
+        print "skip Step:" + skip
+
 def classify(run):
     
-    if run ==True: 
+    if run == True: 
         
-        if helperInstance.checkParameter('Classify', 'program',"parameter.txt") == 'phylosift':
+        if helperInstance.checkParameter('Classify', 'program', "parameter.txt") == 'phylosift':
             print 'starte phylosift'
-        elif helperInstance.checkParameter('Classify', 'program',"parameter.txt") == '':
+        elif helperInstance.checkParameter('Classify', 'program', "parameter.txt") == '':
             print 'starte metacv'
         else:
             print 'no assembly program recognized! Please check parameter file!'
     else:
-        print "skip Step:"+skip
+        print "skip Step:" + skip
+
 def initPipeline():
     
-    preProcessing(helperInstance.skipStep(skip,'PreProcessing'))
-    assembly(helperInstance.skipStep(skip,'Assembly'))
-    annotate(helperInstance.skipStep(skip,'Annotate'))
-    classify(helperInstance.skipStep(skip,'Classify'))
+    preProcessing(helperInstance.skipStep(skip, 'PreProcessing'))
+    assembly(helperInstance.skipStep(skip, 'Assembly'))
+    annotate(helperInstance.skipStep(skip, 'Annotate'))
+    classify(helperInstance.skipStep(skip, 'Classify'))
     
     
 initPipeline()
