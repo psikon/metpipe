d79c600697f27065127f24bdf6a4000c2f90a219
Psikon <psikon@Ubuntu-PC.(none)>
2013-04-23 15:22:49 +0200

    fixed installer

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ while getopts hscq opt
 - Trim Galore!
 [Assembler]
 - MetaVelvet
-- Abyss
 - Velvet
 - concatination of the reads
 [Annotate]
@@ -192,92 +191,6 @@ while true
 		esac
 	done
 
-while true
-	do
-		# checking for install
-		if [ -f program/meta-idba/bin/metaidba ]; then
-			echo "Installation of Meta-IDBA found"
-			break
-		fi
-	
-		if [ "$quiet" = "y" ]; then	
-			CONFIRM="y"
-		else
-			echo -n "Meta-IDBA not found. Download and install it? <y|n> :"
-			read CONFIRM
-		fi
-	case $CONFIRM in
-		y|Y|YES|yes|Yes) 
-			cd program/
-				# Download and uncompress Meta-IDBA assembler
-				wget http://hku-idba.googlecode.com/files/idba-0.19.tar.gz
-				tar xzfv idba-*
-				rm idba-0.19.tar.gz
-				mv idba-* meta-idba
-				cd meta-idba/
-					./configure && make
-			cd ..
-			break;;
-		n|N|no|NO|No)
-			break;
-		;;
-		*) echo Please enter only y or n
-		esac
-	done
-
-while true
-	do
-		if [ -f program/abyss/ABYSS/ABYSS ]; then
-			echo "Installation of Abyss found"
-			break
-		fi
-	
-		if [ "$quiet" = "y" ]; then	
-			CONFIRM="n"
-		else
-			echo -n "Abyss not found. (optional for assembly)\nDownload and install it? <y|n> :"
-			read CONFIRM
-		fi
-	case $CONFIRM in
-		y|Y|YES|yes|Yes) 
-			cd program/
-				mkdir lib
-				cd lib/
-					# Download google sparsehash (required for abyss compilation)
-					wget http://sparsehash.googlecode.com/files/sparsehash-2.0.2.tar.gz
-					tar xzfv sparsehash*
-					rm sparsehash-2.0.2.tar.gz
-					mv sparsehash* sparsehash
-					cd sparsehash/ 
-						# configure and compile google sparsehash
-						./configure && make
-					cd ..
-					# Download and uncompress openmpi (required for multithreading in abyss)
-					wget http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.3.tar.gz
-					tar xzfv openmpi*
-					rm openmpi-1.6.3.tar.gz
-					mv openmpi* openmpi
-					cd openmpi/
-						# configure and compile openmpi
-						./configure && make
-				cd ../..
-				# Download and uncompress abyss
-				wget http://www.bcgsc.ca/downloads/abyss/abyss-1.3.4.tar.gz
-				tar xzfv abyss*
-				rm abyss-1.3.4.tar.gz
-				mv abyss* abyss 
-				cd abyss/
-					# configure abyss for multithreading and MiSeq k-mers (max. 96) and compile
-					./configure '--enable-maxk=96' '--with-mpi=../lib/openmpi/ompi' && make
-			cd ../..
-			break;;
-		n|N|no|NO|No)
-		break;
-		;;
-		*) echo Please enter only y or n
-		esac
-	done
-
 ##### Annotate Tools #####
 
 # Installation of blastn for annotation of the reads
@@ -299,10 +212,10 @@ while true
 		y|Y|YES|yes|Yes) 
 			cd program/
 				# Download and unpack program
-				wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.2.27+-x64-linux.tar.gz
+				wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-*-x64-linux.tar.gz
 				tar xzfv ncbi-blast*
-				rm ncbi-blast-2.2.27+-x64-linux.tar.gz
-				mv ncbi-blast-2.2.27+ blast
+				rm ncbi-blast-2.2.28+-x64-linux.tar.gz
+				mv ncbi-blast-2.2.28+ blast
 			cd ..
 			break;;
 		n|N|no|NO|No)
@@ -521,6 +434,36 @@ while true
 
 echo "\n Checking Installations"
 echo "[Read preprocessing]"
+if [ -f program/fastqc/fastqc ] && [ -f program/trim_galore/trim_galore ]; then
+	echo "Preprocessing tools OK!"
+	ln -s fastqc/fastqc program/quality && chmod 775 program/quality
+	ln -s trim_galore/trim_galore program/filter && chmod 775 program/filter
+fi
+
+if [ -f program/stitch/stitch/stitch.py ]; then
+	echo "Assembly - Concatenation OK!"
+	ln -s stitch/stitch/stitch.py program/concat && chmod 775 program/concat
+fi	
+
+if [ -f program/velvet/velveth ] && [ -f program/velvet/velvetg ] && [ -f program/metavelvet/meta-velvetg ]; then
+	echo "Assembly - MetaVelvet OK!"
+	ln -s velvet/velveth program/velveth && chmod 775 program/velveth
+	ln -s velvet/velvetg program/velvetg && chmod 775 program/velvetg
+	ln -s metavelvet/meta-velvetg program/metavelvet && chmod 775 program/metavelvet
+fi
+
+if [ -f program/blast/bin/blastn ] && [ -f program/db/nt.nal ]; then
+	echo "Annotate - Blastn OK!"
+	ln -s blast/bin/blastn program/blast && chmod 775 program/blast
+fi
+
+if  [ -f program/metacv/metacv ] && [ -f program/db/cvdb_2059/ 9; then
+	echo "Annotate - MetaCV OK!"
+	ln -s metacv/metacv program/metacv && chmod 775 program/metacv
+fi
+
+
+
 	
 
 
