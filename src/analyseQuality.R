f79e3a4871693ee48242345dd1c19a1a92037704
Philipp Sehnert <psikon86@gmail.com>
2013-05-16 15:08:15 +0200

    initial creation of testing

--- src/analyseQuality.R
+++
@@ -1,8 +0,0 @@
-args <- commandArgs(trailingOnly=T)
-#args <- "SPICEIII/miseq/scripts/metpipe/result/RAW/forward_fastqc/fastqc_data.txt"
-lines <- readLines(args)
-encoding <- lines[grep("Encoding",lines)]
-idx <- c(grep(">>Per base sequence quality",lines)+1,grep(">>END_MODULE",lines)[2]-1)
-quality <- read.table(textConnection(lines[idx[1]:idx[2]]),sep="\t",header=F)
-
-c(tail(quality$V2,n=1),encoding)
